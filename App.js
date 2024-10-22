f5133b06a8982d808c691e2009a19966da39768b
Blessings <blessingsx99@gmail.com>
2024-10-22 20:44:03 +0200

    Everything works fine from the hackathon

--- App.js
+++ App.js
@@ -3,10 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Registration from './screens/Registration';
-import PersonalInfoForm from './screens/PersonalInfoForm';
-import ContactDetailsScreen from './screens/ContactDetailsScreen'; // Adjust the path based on where your file is located
-import VerifyEmailScreen from './screens/VerifyEmailScreen';
-import SuccessScreen from './screens/RegistrationSuccessScreen';
 import TermsScreen from './screens/TermsScreen';
 import Login from './screens/Login';
 import Index from './screens/Index';
@@ -28,10 +24,6 @@ export default function App() {
         <Stack.Screen name="People" component={People} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         <Stack.Screen name="Registration" component={Registration} options={{ headerShown: false }} />
-        <Stack.Screen name="PersonalInfo" component={PersonalInfoForm} options={{ headerShown: false }} />
-        <Stack.Screen name="ContactDetails" component={ContactDetailsScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="VerifyEmail" component={VerifyEmailScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Success" component={SuccessScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Terms" component={TermsScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
       
